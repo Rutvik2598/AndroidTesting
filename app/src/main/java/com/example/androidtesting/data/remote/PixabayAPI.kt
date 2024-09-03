d1fe1a0eb6ee37425e050105de041a0185f74e51
Rutvik <kulkarnirutvikvishwas@gmail.com>
2024-09-03 00:19:52 -0400

    Setting up dagger hilt

---
+++ app/src/main/java/com/example/androidtesting/data/remote/PixabayAPI.kt
@@ -0,0 +1,16 @@
+package com.example.androidtesting.data.remote
+
+import com.example.androidtesting.BuildConfig
+import com.example.androidtesting.data.remote.responses.ImageResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixabayAPI {
+
+    @GET("/api/")
+    suspend fun searchForImage(
+        @Query("q") searchQuery: String,
+        @Query("key") apiKey: String = BuildConfig.API_KEY
+    ): Response<ImageResponse>
+}
---
+++ app/src/main/java/com/example/androidtesting/other/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.androidtesting.other
+
+object Constants {
+
+    val DATABASE_NAME = "shopping_db"
+
+    val BASE_URL = "https://pixabay.com"
+}
